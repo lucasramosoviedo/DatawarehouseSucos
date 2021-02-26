1db21be6cf133d627692f0bb9f164536867a02ae
Lucas Ramos do Prado Oviedo <luc_supra@hotmail.com>
2021-02-26 11:32:33 -0300

    Atualização das dimensões e tabelas de fato

---
+++ Tables/Dim_Cliente.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Dim_Cliente]
+(
+	[Cod_Cliente] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Desc_Cliente] NVARCHAR(200) NULL,
+    [Cod_Cidade] NCHAR(10) NULL,
+    [Desc_Cidade] NVARCHAR(200) NULL,
+    [Cod_Estado] NCHAR(10) NULL,
+    [Desc_Estado] NVARCHAR(200) NULL,
+    [Cod_Regiao] NCHAR(10) NULL,
+    [Desc_Regiao] NVARCHAR(200) NULL,
+    [Cod_Segmento] NCHAR(10) NULL,
+    [Desc_Segmento] NVARCHAR(200) NULL    
+)
---
+++ Tables/Fato_004.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Fato_004]
+(
+	[Cod_Cliente] NVARCHAR(50) NOT NULL , 
+    [Cod_Produto] NVARCHAR(50) NOT NULL, 
+    [Cod_Organizacional] NVARCHAR(50) NOT NULL, 
+    [Cod_Dia] NVARCHAR(50) NOT NULL, 
+    [Meta_Faturamento] FLOAT NULL, 
+    CONSTRAINT [FK_Fato_004_Dim_Cliente] FOREIGN KEY (Cod_Cliente) REFERENCES Dim_Cliente(Cod_Cliente), 
+    CONSTRAINT [FK_Fato_004_Dim_Produto] FOREIGN KEY (Cod_Produto) REFERENCES Dim_Produto(Cod_Produto), 
+    CONSTRAINT [FK_Fato_004_Dim_Orgazacional] FOREIGN KEY (Cod_Organizacional) REFERENCES Dim_Organizacional(Cod_Filho), 
+    CONSTRAINT [FK_Fato_004_Dim_Tempo] FOREIGN KEY (Cod_Dia) REFERENCES Dim_Tempo(Cod_Dia),
+    PRIMARY KEY ([Cod_Cliente], [Cod_Produto], [Cod_Organizacional], [Cod_Dia])
+    
+)
