9baa1e05354f6491aa16dde47d0e4814f2bc795e
Lucas Ramos do Prado Oviedo <luc_supra@hotmail.com>
2021-03-04 16:48:25 -0300

    Alteração na dimensão cliente

--- Tables/Dim_Cliente.sql
+++ Tables/Dim_Cliente.sql
@@ -2,12 +2,12 @@
 (
 	[Cod_Cliente] NVARCHAR(50) NOT NULL PRIMARY KEY, 
     [Desc_Cliente] NVARCHAR(200) NULL,
-    [Cod_Cidade] NCHAR(10) NULL,
+    [Cod_Cidade] NVARCHAR(50) NULL,
     [Desc_Cidade] NVARCHAR(200) NULL,
-    [Cod_Estado] NCHAR(10) NULL,
+    [Cod_Estado] NVARCHAR(50) NULL,
     [Desc_Estado] NVARCHAR(200) NULL,
-    [Cod_Regiao] NCHAR(10) NULL,
+    [Cod_Regiao] NVARCHAR(50) NULL,
     [Desc_Regiao] NVARCHAR(200) NULL,
-    [Cod_Segmento] NCHAR(10) NULL,
+    [Cod_Segmento] NVARCHAR(50) NULL,
     [Desc_Segmento] NVARCHAR(200) NULL    
 )
